7b98c925efab92f8f23f3073b1e3acb5b714a9fd
Marc Wenzlawski <llbbbmn.wenzlawski@gmail.com>
2019-12-28 17:46:55 +0100

    Added timer file

---
+++ timer.py
@@ -0,0 +1,17 @@
+def calculate_time(func):
+
+    def inner(**kw):
+
+        # Add timer
+
+        func(**kw)
+
+        # Add timer
+
+def count_swaps():
+    pass
+    # Function to count how many times elements were swapped during the sorting of an array
+
+def count_comps():
+    pass
+    # Function to count how many times two elements are compared during the sorting of an array
